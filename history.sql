147d8ff4751c379c56426405a3a285b5f229afaf
CCAndyVV <83277724+CCAndyVV@users.noreply.github.com>
2021-05-18 19:55:02 +0800

    Update history.sql

--- history.sql
+++ history.sql
@@ -1,12 +1,7 @@
 CREATE TABLE IF NOT EXISTS history (
-  userid VARCHAR(255) NOT NULL,
-  city VARCHAR(4) NOT NULL,
-  district VARCHAR(6) NOT NULL,
   name VARCHAR(255),
-  trade_date int NOT NULL,
-  highest_price bigint NOT NULL,
-  lowest_price bigint NOT NULL,
-  PRIMARY KEY (userid, city, district, trade_date, highest_price, lowest_price)
+  rate int NOT NULL,
+  PRIMARY KEY (name, rate)
   );
 
 DELETE FROM sys.dm_exec_cached_plans AS p
