e8a8a7bcd2ea2b8400c47d9d06cd0f9994d99057
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2021-05-22 17:49:40 +0800

    update init.sql, change id to increment.

--- init.sql
+++ init.sql
@@ -4,21 +4,13 @@ ALTER DATABASE term_project CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 use term_project;
 
 CREATE TABLE IF NOT EXISTS userlis( 
-    userid VARCHAR(255) NOT NULL, 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     email VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL,
-    admin boolean NOT NULL,
-    PRIMARY KEY (email)
+    admin boolean NOT NULL
 );
 
-create table restaurant(
-    name VARCHAR(255),
-    rating float,
-    user_ratings_total int,
-    address VARCHAR(255),
-    latitude float,
-    longitude float
-);
+ 
 
 CREATE TABLE IF NOT EXISTS history (
   userid int NOT NULL,
