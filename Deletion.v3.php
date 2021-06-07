8ab9147947cc79349b14445b5c5996fd018f1a32
CCAndyVV <83277724+CCAndyVV@users.noreply.github.com>
2021-06-08 00:12:33 +0800

    Update Deletion.v3.php

--- Deletion.v3.php
+++ Deletion.v3.php
@@ -47,27 +47,14 @@ function db_conn() {
 
 
 
-  // <?php
-  // // 假定数据库用户名：root，密码：123456，数据库：RUNOOB
-  // $con=mysqli_connect("localhost","root","123456","RUNOOB");
-  // if (mysqli_connect_errno($con))
-  // {
-  //     echo "连接 MySQL 失败: " . mysqli_connect_error();
-  // }
-  //
-  // // 执行查询
-  // mysqli_query($con,"SELECT * FROM websites");
-  // mysqli_query($con,"INSERT INTO websites (name, url, alexa, country)
-  // VALUES ('百度','https://www.baidu.com/','4','CN')");
-  //
-  // mysqli_close($con);
+  
 
 
 
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   $conn = db_conn();
 
-  //$query = "SELECT userid, city FROM history";
+  
 
   $result = mysqli_query($conn, "SELECT userid, city FROM history");
 
